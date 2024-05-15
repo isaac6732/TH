c6ba7cf87a48c8faccda8eacfa61a244f9587ce1
Isaac Oquendo <ioalvarez378@gmail.com>
2024-05-15 16:33:32 -0400

    TTH

---
+++ TH/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+using TH.Models;
+
+namespace TH
+{
+	public interface IProductRepository
+	{
+		public IEnumerable<Product> GetAllProducts();
+
+		public Product GetProduct(int id);
+
+		public void UpdateProduct(Product product);
+
+		public void InsertProduct(Product productToInsert);
+
+		public IEnumerable<Category> GetCategories();
+
+		public Product AssignCategory();
+
+		public void DeleteProduct(Product product);
+	}
+}
